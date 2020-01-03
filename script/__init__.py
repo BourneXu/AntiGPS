c1397e69caf9da69028c28e211c0be5cae26a640
Chao <chaoxu18@vt.edu>
2020-01-02 21:03:43 -0500

    Add: analysis and decider

---
+++ script/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+###
+# @Author: Chris
+# Created Date: 2020-01-02 17:03:06
+# -----
+# Last Modified: 2020-01-02 17:03:09
+# Modified By: Chris
+# -----
+# Copyright (c) 2020
+###
+
--- script/utility.py
+++ script/utility.py
@@ -1,5 +1,20 @@
+# -*- coding:utf-8 -*-
 import io
+import os
+import scipy
+import random
+import geocoder
+import numpy as np
 from PIL import Image
+from scipy import signal
+from dynaconf import settings
+import matplotlib.pyplot as plt
+from statsmodels.distributions.empirical_distribution import ECDF
+
+NORTHERNMOST = 49.0
+SOUTHERNMOST = 25.0
+EASTERNMOST = -66.0
+WESTERNMOST = -124.0
 
 
 class Utility:
@@ -7,3 +22,24 @@ class Utility:
     def image_display(image: bytes):
         image = Image.open(io.BytesIO(image))
         image.show()
+
+    @staticmethod
+    def plot_cdf(xarray):
+        # xarray_sorted = np.sort(xarray)
+        # percent = 1.0 * np.arange(len(xarray)) / (len(xarray) - 1)
+        # line = plt.plot(xarray_sorted, percent)
+
+        ecdf = ECDF(xarray)
+        line = plt.plot(ecdf.x, ecdf.y * 100)
+        return line
+
+    @staticmethod
+    def generateGPS_random(number_of_points):
+        coordinate_list = []
+        for _ in range(number_of_points):
+            lat = round(random.uniform(SOUTHERNMOST, NORTHERNMOST), 6)
+            lng = round(random.uniform(EASTERNMOST, WESTERNMOST), 6)
+            gcode = geocoder.mapbox([lat, lng], method="reverse", key=settings.MAPTOKEN)
+            coordinate_list.append(gcode)
+        return coordinate_list
+
